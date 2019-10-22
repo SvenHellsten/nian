4ca7f034fe98272e6606409696fcdc1858f5e775
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2019-10-22 14:09:31 +0200

    added lowercase thingy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var data = require('./your_file.json');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      nian: 'Skriv in nio bokstäver för att se om det är ett ord.',
+    };
+
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.setState({nian: ''})
+    if (event.target.value.length == 9){
+      if (data.includes(event.target.value.toLowerCase())){
+        this.setState({nian: event.target.value+' ÄR ett nianord!'})
+        }else{
+          this.setState({nian: event.target.value.toLowerCase()+' ÄR INTE ett nianord...'})
+        }
+      
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+            <textarea value={this.state.value} onChange={this.handleChange} cols={40} rows={4} />
+        </form>
+        <div className="preview">
+          <h1>{this.state.nian}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
