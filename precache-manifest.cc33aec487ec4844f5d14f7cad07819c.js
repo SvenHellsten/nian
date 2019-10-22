078266607e3210fe1838d234062c212b69a16056
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2019-10-22 11:14:55 +0200

    Updates

---
+++ precache-manifest.cc33aec487ec4844f5d14f7cad07819c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7c7974f9cc64063a2acea3c624f1da",
+    "url": "/nian/index.html"
+  },
+  {
+    "revision": "01f2817d1de0d11eaddc",
+    "url": "/nian/static/css/main.f95c0f22.chunk.css"
+  },
+  {
+    "revision": "4e86efc92fdee9876003",
+    "url": "/nian/static/js/2.58fc6c33.chunk.js"
+  },
+  {
+    "revision": "01f2817d1de0d11eaddc",
+    "url": "/nian/static/js/main.49bcacb8.chunk.js"
+  },
+  {
+    "revision": "5371837dd30a98ccf654",
+    "url": "/nian/static/js/runtime-main.e5bd2252.js"
+  }
+]);
